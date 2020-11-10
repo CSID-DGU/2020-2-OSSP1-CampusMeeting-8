e45636a67452b0c57388104cadd1be3ff65e37fc
guno-kim <64324992+guno-kim@users.noreply.github.com>
2020-11-11 00:49:15 +0900

    button refactoring

--- public/js/util.js
+++ public/js/util.js
@@ -25,37 +25,30 @@ let screenConstraint = {
 
 cameraBtn.addEventListener('click', () => {
     let localStream = participants[socket.id].rtcPeer.getLocalStream();
-    console.log(nowVideoMode);
-    if (nowVideoMode == videoMode.cameraOn) {
-        nowVideoMode = videoMode.cameraOff;
-        localStream.getVideoTracks()[0].enabled = false;
+    switch (nowVideoMode) {
+        case videoMode.cameraOn:
+            nowVideoMode = videoMode.cameraOff;
+            localStream.getVideoTracks()[0].enabled = false;
+            break;
+        case videoMode.cameraOff:
+            nowVideoMode = videoMode.cameraOn;
+            localStream.getVideoTracks()[0].enabled = true;
+            break;
+        case videoMode.screenOff: {
+            nowVideoMode = videoMode.cameraOn;
+            let oldTrack = localStream.getVideoTracks()[0];
+            localStream.removeTrack(oldTrack);
+            getCameraTrack(cameraConstraint).then(track => localStream.addTrack(track));
+            break;
+        }
+        case videoMode.screenOn:
+            nowVideoMode = videoMode.cameraOn;
+            let oldTrack = localStream.getVideoTracks()[0];
+            localStream.removeTrack(oldTrack);
+            getCameraTrack(cameraConstraint).then(track => localStream.addTrack(track));
+            break;
     }
-    else if (nowVideoMode == videoMode.cameraOff) {
-        nowVideoMode = videoMode.cameraOn;
-        localStream.getVideoTracks()[0].enabled = true;
-    }
-    else if (nowVideoMode == videoMode.screenOff) {
-        nowVideoMode = videoMode.cameraOn;
-        let oldTrack = localStream.getVideoTracks()[0];
-        localStream.removeTrack(oldTrack);
-        navigator.mediaDevices.getUserMedia(cameraConstraint)
-            .then((_stream) => {
-                let newTrack = _stream.getVideoTracks()[0];
-                localStream.addTrack(newTrack);
 
-            })
-    }
-    else if (nowVideoMode == videoMode.screenOn) {
-        nowVideoMode = videoMode.cameraOn;
-        let oldTrack = localStream.getVideoTracks()[0];
-        localStream.removeTrack(oldTrack);
-        navigator.mediaDevices.getUserMedia(cameraConstraint)
-            .then((_stream) => {
-                let newTrack = _stream.getVideoTracks()[0];
-                localStream.addTrack(newTrack);
-
-            })
-    }
 })
 
 
@@ -75,36 +68,32 @@ screenBtn.addEventListener('click', () => {
             nowVideoMode = videoMode.screenOn;
             let oldTrack = localStream.getVideoTracks()[0];
             localStream.removeTrack(oldTrack);
-            navigator.mediaDevices.getDisplayMedia(screenConstraint)
-                .then((_stream) => {
-                    let newTrack = _stream.getVideoTracks()[0];
-                    localStream.addTrack(newTrack);
-                })
+            getScreenTrack(screenConstraint).then(track => localStream.addTrack(track));
             break;
         }
         case videoMode.cameraOn:
             nowVideoMode = videoMode.screenOn;
             let oldTrack = localStream.getVideoTracks()[0];
             localStream.removeTrack(oldTrack);
-            navigator.mediaDevices.getDisplayMedia(screenConstraint)
-                .then((_stream) => {
-                    let newTrack = _stream.getVideoTracks()[0];
-                    localStream.addTrack(newTrack);
-                })
+            getScreenTrack(screenConstraint).then(track => localStream.addTrack(track));
             break;
     }
 })
 
 function getCameraTrack(constraint) {
-    navigator.mediaDevices.getUserMedia(constraint)
-        .then((_stream) => {
-            return _stream.getVideoTracks()[0];
-        })
+    return new Promise((resolve, reject) => {
+        navigator.mediaDevices.getUserMedia(constraint)
+            .then((_stream) => {
+                resolve(_stream.getVideoTracks()[0]);
+            })
+    })
 }
 function getScreenTrack(constraint) {
-    navigator.mediaDevices.getDisplayMedia(constraint)
-        .then((_stream) => {
-            return _stream.getVideoTracks()[0];
-        })
+    return new Promise((resolve, reject) => {
+        navigator.mediaDevices.getDisplayMedia(constraint)
+            .then((_stream) => {
+                resolve(_stream.getVideoTracks()[0]);
+            })
+    })
 }
 
