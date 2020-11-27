b7f282a9867cac7d3d67554f89d346d703ca7d47
Song <sych7389@dgu.ac.kr>
2020-11-27 14:54:16 +0900

    session 이용한 로그아웃 추가, 몇 페이지 라우팅 수정

--- login/index.js
+++ login/index.js
@@ -1,15 +1,23 @@
 var express=require("express");
 var bodyParser=require('body-parser');
-
 var connection = require('./config');
 var app = express();
-
 var authController=require('./controllers/auth-controller');
 var registerController=require('./controllers/register-controller');
+var session = require('express-session');
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+
+
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
@@ -18,7 +26,40 @@ app.get('/login', function (req, res) {
    res.sendFile( __dirname + "/" + "login.html" );
 })
 
-/* route to handle login and registration */
+app.get('/register', function (req, res) {
+   res.sendFile( __dirname + "/" + "register.html" );
+})
+app.get('/login.css',function(req,res){
+  res.sendFile(__dirname + "/" + "login.css");
+})
+app.get('/main', function (req, res) {
+   if(!req.session.user) res.redirect('/login');
+   else {
+     console.log(req.session.user.id);
+     res.sendFile(__dirname + "/" + "main.html");
+   }
+})
+
+app.get("/logout",function(req,res){
+  if(req.session.user){
+    req.session.destroy(
+      function (err){
+        if(err){
+          console.log("log out session error");
+          return;
+        }
+        console.log("log out");
+        res.redirect("/");
+      }
+    );
+
+  } else{
+    console.log("no session");
+    res.redirect('/login');
+  }
+
+})
+
 app.post('/api/register',registerController.register);
 app.post('/api/auth',authController.auth);
 
@@ -26,3 +67,5 @@ console.log(authController);
 app.post('/controllers/register-controller', registerController.register);
 app.post('/controllers/auth-controller', authController.auth);
 app.listen(3000);
+
+module.exports = app;
