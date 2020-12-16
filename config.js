cb83b3d9d4fd2b96f2cb71921f346cfd5a42f9a2
Oh <clover7even@naver.com>
2020-12-16 10:51:06 +0900

    config.js 변경

--- config.js
+++ config.js
@@ -1,17 +1,13 @@
 const mysql = require('mysql');
+
 // 로컬 DB 사용시 해당 부분을 mysql 설정에 맞춰서 변경
-/* const connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '1234',
-  database : 'test'
-}); */
-const connection = mysql.createConnection({
-  host: 'database-1.cg3niva28v4j.ap-northeast-2.rds.amazonaws.com',
-  user: 'admin',
-  password: 'webrtc1234',
-  database: 'userinfo'
+  database : 'userinfo',
 });
+
 connection.connect(function (err) {
   if (!err) {
     console.log("Database is connected");
